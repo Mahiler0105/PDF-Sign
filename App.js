442253f0ac3d5763cecb4928767a3b4da1ab56ca
Fernando Mahiler Chullo Mamani <fernando.mahiler@gmail.com>
2021-08-18 18:21:43 -0500

    [UPDATE]: Push notification Android

--- App.js
+++ App.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import Application from './src/index';
+import {RNNotificationBanner} from 'react-native-notification-banner';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import messaging from '@react-native-firebase/messaging';
+import Application from './src/index';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+let icon = (
+  <Icon family={'FontAwesome'} name={'edit'} color={'#000000'} size={30} />
+);
 
 const App = () => {
+  React.useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      const {
+        notification: {body: subTitle, title},
+      } = remoteMessage;
+      RNNotificationBanner.Show({
+        title,
+        subTitle,
+        withIcon: true,
+        icon,
+        titleColor: '#FFFFFF',
+        subTitleColor: '#FFFFFF',
+        isSwipeToDismissEnabled: true,
+        tintColor: '#eaa2a2',
+      });
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Application />
